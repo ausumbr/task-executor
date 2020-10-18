85e253cf61cf886645bf84a25e3732512e146aa2
Tiago Resende <tiagor87@gmail.com>
2020-10-17 23:41:10 -0300

    :fire: Remove usings não utilizados.

--- src/Core/TaskExecutor.cs
+++ src/Core/TaskExecutor.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
